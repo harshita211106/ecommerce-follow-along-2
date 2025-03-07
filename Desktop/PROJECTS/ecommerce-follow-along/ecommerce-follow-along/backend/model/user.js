c8a3c3632ef5b31dbcf050543b3fe361a0ddd551
harshita211106 <114283416+harshita211106@users.noreply.github.com>
2025-03-07 13:20:40 +0530

    work dont till milestone 4

---
+++ Desktop/PROJECTS/ecommerce-follow-along/ecommerce-follow-along/backend/model/user.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  profilePicture: { type: String }, // Will store the file path of uploaded images
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
---
+++ Desktop/PROJECTS/ecommerce-follow-along/ecommerce-follow-along/frontend/routes/routes.jsx
@@ -0,0 +1,9 @@
+
+import Login from "../pages/Login.jsx";
+import Signup from "../pages/Signup.jsx";
+import Home from "../pages/Home.jsx"
+import CreateProduct from "../pages/createProduct.jsx";
+import MyProducts from "../pages/myProducts.js";
+import Cart from "../pages/cart.jsx";
+import ProductDetails from "../pages/productDetails.jsx";
+export { Login,Signup,Home, CreateProduct, MyProducts, Cart, ProductDetails}
